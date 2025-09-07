f500d7898f2be30cf621794e2b0c07acb387e2f1
thaontpla <thaontp.la@gmail.com>
2025-09-07 11:50:03 +0700

    Login test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://accounts.saucelabs.com/am/XUI/#login/");
+        driver.get("https://www.saucedemo.com/");
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='user-name']"));
+        usernameInput.sendKeys("standard_user");
+        WebElement pwInput = driver.findElement(By.xpath("//input[@id='password']"));
+        pwInput.sendKeys("secret_sauce");
+        WebElement btnInput = driver.findElement(By.xpath("//input[@id='login-button']"));
+        btnInput.click();
         driver.close();
     }
 }
